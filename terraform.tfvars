c713fe6b300562cd357a77e5a3aa8ad04451723e
Genevieve Bita <genevievebita@yahoo.com>
2022-09-04 09:05:57 -0500

    new

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+region        = "us-east-1"
+ami           = "ami-05fa00d4c63e32376"
+keypair       = "RR"
+instance-type = "t2-nano"
---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "ami" {
+  type    = string
+  default = "ami-05fa00d4c63e32376"
+}
+
+variable "instance-type" {
+   type    = string
+   default = "t2-nano"
+}
+
+variable "keypair" {
+   type    = string
+   default = "RR"
+}
